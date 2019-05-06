339c0716c310def85a8543ae391343026d174330
phongcampboy <48239635+phongcampboy@users.noreply.github.com>
2019-05-06 14:53:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,9 @@ if (!is_null($events['events'])) {
 
    // Make a POST Request to Messaging API to reply to sender
    $url = 'https://api.line.me/v2/bot/message/reply';
-   $data = 'suwaphong';
+   $data = [
+    'replyToken' => $replyToken,
+    'messages' => [$messages],
    $post = json_encode($data);
    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
