83a9145ede7c6aa0cb9007655cf7ece182341ae7
phongcampboy <48239635+phongcampboy@users.noreply.github.com>
2019-05-06 14:54:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,6 +24,7 @@ if (!is_null($events['events'])) {
 
    // Make a POST Request to Messaging API to reply to sender
    $url = 'https://api.line.me/v2/bot/message/reply';
+   $replyToken='phong];
    $data = [
     'replyToken' => $replyToken,
     'messages' => [$messages],
