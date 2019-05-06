84dbcc1789b7939f69f33dbee94b0aa3f15c0703
phongcampboy <48239635+phongcampboy@users.noreply.github.com>
2019-05-06 13:48:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,7 @@ if (!is_null($events['events'])) {
    $text = $event['message']['text'];
    // Get replyToken
    $replyToken = $event['replyToken'];
-   echo $replyToken;
-
+   
    // Build message to reply back
    $messages = [
     'type' => 'text',
@@ -26,7 +25,7 @@ if (!is_null($events['events'])) {
    // Make a POST Request to Messaging API to reply to sender
    $url = 'https://api.line.me/v2/bot/message/reply';
    $data = [
-    'replyToken' => $replyToken,
+    'TMN' => $replyToken,
     'messages' => [$messages],
    ];
    $post = json_encode($data);
